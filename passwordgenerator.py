0a018043eb05a8088ec4e35369d01d0e7e85421c
Sean Lester Durham <sean.durham1982@gmail.com>
2023-06-12 17:40:25 -0500

    created python password generator

---
+++ passwordgenerator.py
@@ -0,0 +1,25 @@
+import random
+
+
+print('Welcome to Your Password Generator')
+
+
+chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZXabcdefghijklmnopqrstuvwxyz!@#$%^&*().,?0123456789'
+
+
+number = input('Amount of passwords to generate: ')
+
+number = int(number)
+
+length = input('Input Your password length: ')
+length = int(length)
+
+print('\nhere are your passwords: ')
+
+
+
+for pwd in range(number):
+    passwords =''
+    for c in range(length):
+        passwords += random.choice(chars)
+        print(passwords)
